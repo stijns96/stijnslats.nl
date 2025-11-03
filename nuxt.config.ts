f488ef8b4c340c400efd1b61c3ad84cc58c235d5
Stijn <stijn@code.nl>
2025-11-03 20:04:43 +0100

    Add ESLint configuration and integrate Nuxt ESLint module; update package.json and pnpm-lock.yaml for new dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
 });
