3e606d34703da48ba4b2e0c1316576a12e8cf7a7
Stijn <stijn@code.nl>
2025-11-04 18:57:37 +0100

    Enhance app configuration by defining button variants and page card styles; update primary color setting for UI components.

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,39 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      // primary: "blue",
+      primary: "brand",
       // neutral: "slate",
+    },
+    button: {
+      slots: { base: ["rounded-full"] },
+      variants: {
+        size: {
+          md: {
+            base: "px-10 py-5",
+          },
+        },
+      },
+      compoundVariants: [
+        {
+          color: "primary",
+          variant: "solid",
+          class:
+            "text-default bg-linear-to-r from-[#5e31db] to-[#2473eb] hover:from-[#b493f1] hover:to-[#29288b] duration-1000",
+        },
+      ],
     },
     pageHero: {
       slots: {
         title: "font-medium md:text-8xl",
+      },
+    },
+    pageCard: {
+      variants: {
+        variant: {
+          subtle: {
+            root: "bg-muted/25 ring-primary",
+          },
+        },
       },
     },
   },
